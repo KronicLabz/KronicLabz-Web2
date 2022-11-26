5f385a3a3de581e8c31c593825cc3f4b74e49d40
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-11-26 05:22:12 -0600

    Add files via upload

--- src/common/Layout.js
+++ src/common/Layout.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import HeaderOne from "./header/HeaderOne";
-import FooterTwo from "./footer/FooterTwo";
-import Copyright from "./footer/Copyright";
 
 const Layout = ({children}) => {
     return (
@@ -9,8 +7,6 @@ const Layout = ({children}) => {
             <main className="page-wrapper">
                 <HeaderOne btnStyle="btn-small round btn-icon" HeaderSTyle="header-not-transparent" />
                 {children}
-                <FooterTwo />
-                <Copyright />
             </main>
         </>
     )
