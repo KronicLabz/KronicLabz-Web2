2abe24de4e7bf37b7ae36c141b0b0c135b38e594
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2022-12-27 08:43:19 -0600

    Add files via upload

--- src/elements/pricing/PricingOne.js
+++ src/elements/pricing/PricingOne.js
@@ -9,26 +9,24 @@ const PricingOne = () => {
                 <div className="rn-pricing">
                     <div className="pricing-table-inner">
                         <div className="pricing-header">
-                            <h4 className="title">Free</h4>
+                            <h4 className="title">Standard Collection Drop</h4>
                             <div className="pricing">
                                 <div className="price-wrapper">
                                     <span className="currency">$</span>
-                                    <span className="price">29</span>
+                                    <span className="price">1,350</span>
                                 </div>
-                                <span className="subtitle">USD Per Month</span>
+                                <span className="subtitle">Great for traditional art and music</span>
                             </div>
                         </div>
                         <div className="pricing-body">
                             <ul className="list-style--1">
-                                <li><FiCheck /> 5 PPC Campaigns</li>
-                                <li><FiCheck /> Digital Marketing</li>
-                                <li><FiCheck /> Marketing Agency</li>
-                                <li><FiCheck /> Seo Friendly</li>
-                                <li><FiCheck /> UI/UX designs</li>
+                                <li><FiCheck /> Audited ERC721 Contract</li>
+                                <li><FiCheck /> Contract Dashboard</li>
+                                <li><FiCheck /> Minting Page</li>
                             </ul>
                         </div>
                         <div className="pricing-footer">
-                            <a className="btn-default btn-border" href="#pricing">Purchase Now</a>
+                            <a className="btn-default btn-border" href="./contact">Learn More</a>
                         </div>
                     </div>
                 </div>
@@ -40,26 +38,26 @@ const PricingOne = () => {
                 <div className="rn-pricing active">
                     <div className="pricing-table-inner">
                         <div className="pricing-header">
-                            <h4 className="title">Business</h4>
+                            <h4 className="title">Pro NFT Degen</h4>
                             <div className="pricing">
                                 <div className="price-wrapper">
                                     <span className="currency">$</span>
-                                    <span className="price">50</span>
+                                    <span className="price">2,100</span>
                                 </div>
-                                <span className="subtitle">USD Per Month</span>
+                                <span className="subtitle">Great for web3 businesses</span>
                             </div>
                         </div>
                         <div className="pricing-body">
                             <ul className="list-style--1">
-                                <li><FiCheck /> 5 PPC Campaigns</li>
-                                <li><FiCheck /> Keep 100% Royalties</li>
-                                <li><FiCheck /> App Development</li>
-                                <li><FiCheck /> Seo Friendly</li>
-                                <li><FiCheck /> UI/UX designs</li>
+                                <li><FiCheck /> ERC721A Smart Contract</li>
+                                <li><FiCheck /> DEEP Dive Security Audit Report</li>
+                                <li><FiCheck /> Project Github Page</li>
+                                <li><FiCheck /> Minting Dapp</li>
+                                <li><FiCheck /> Custom Contract Dashboard</li>
                             </ul>
                         </div>
                         <div className="pricing-footer">
-                            <a className="btn-default" href="#pricing">Purchase Now</a>
+                            <a className="btn-default" href="./contact">Learn More</a>
                         </div>
                     </div>
                 </div>
@@ -71,26 +69,24 @@ const PricingOne = () => {
                 <div className="rn-pricing">
                     <div className="pricing-table-inner">
                         <div className="pricing-header">
-                            <h4 className="title">Advanced</h4>
+                            <h4 className="title">Alpha NFT Drop</h4>
                             <div className="pricing">
                                 <div className="price-wrapper">
                                     <span className="currency">$</span>
-                                    <span className="price">100</span>
+                                    <span className="price">5,100</span>
                                 </div>
-                                <span className="subtitle">USD Per Month</span>
+                                <span className="subtitle">Includes all of Pro NFT Degen package plus:</span>
                             </div>
                         </div>
                         <div className="pricing-body">
                             <ul className="list-style--1">
-                                <li><FiCheck /> 50 PPC Campaigns</li>
-                                <li><FiCheck /> SEO Marketing</li>
-                                <li><FiCheck /> Marketing Agency</li>
-                                <li><FiCheck /> Seo Friendly</li>
-                                <li><FiCheck /> Application Manage</li>
+                                <li><FiCheck /> Stakable ERC721A</li>
+                                <li><FiCheck /> NFT Staking Contract</li>
+                                <li><FiCheck /> Staking Dapp</li>
                             </ul>
                         </div>
                         <div className="pricing-footer">
-                            <a className="btn-default btn-border" href="#pricing">Purchase Now</a>
+                            <a className="btn-default btn-border" href="./contact">Learn More</a>
                         </div>
                     </div>
                 </div>
--- src/elements/pricing/PricingTwo.js
+++ src/elements/pricing/PricingTwo.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import { FiCheck } from "react-icons/fi";
 
-const PricingTwo = () => {
+const PricingOne = () => {
     return (
-        <div className="row row--15">
-
+        <div className="row row--30">
             {/* Start PRicing Table Area  */}
             <div className="col-lg-4 col-md-6 col-12">
-                <div className="rn-pricing style-2">
+                <div className="rn-pricing">
                     <div className="pricing-table-inner">
                         <div className="pricing-header">
-                            <h4 className="title">Free</h4>
+                            <h4 className="title">ERC20 Rewards Token</h4>
                             <div className="pricing">
                                 <div className="price-wrapper">
                                     <span className="currency">$</span>
-                                    <span className="price">29</span>
+                                    <span className="price">900</span>
                                 </div>
-                                <span className="subtitle">USD Per Month</span>
+                                <span className="subtitle">Non-Liquid staking rewards token</span>
                             </div>
                         </div>
                         <div className="pricing-body">
                             <ul className="list-style--1">
-                                <li><FiCheck /> 5 PPC Campaigns</li>
-                                <li><FiCheck /> Digital Marketing</li>
-                                <li><FiCheck /> Marketing Agency</li>
-                                <li><FiCheck /> Seo Friendly</li>
-                                <li><FiCheck /> UI/UX designs</li>
+                                <li><FiCheck /> Audited ERC20 Contract</li>
+                                <li><FiCheck /> Contract Dashboard</li>
+                                <li><FiCheck /> Minting Page</li>
                             </ul>
                         </div>
                         <div className="pricing-footer">
-                            <a className="btn-default btn-border" href="#pricing">Purchase Now</a>
+                            <a className="btn-default btn-border" href="./contact">Learn More</a>
                         </div>
                     </div>
                 </div>
@@ -38,29 +35,29 @@ const PricingTwo = () => {
 
             {/* Start PRicing Table Area  */}
             <div className="col-lg-4 col-md-6 col-12">
-                <div className="rn-pricing style-2 active">
+                <div className="rn-pricing active">
                     <div className="pricing-table-inner">
                         <div className="pricing-header">
-                            <h4 className="title">Business</h4>
+                            <h4 className="title">ERC Asset Token</h4>
                             <div className="pricing">
                                 <div className="price-wrapper">
                                     <span className="currency">$</span>
-                                    <span className="price">50</span>
+                                    <span className="price">2,200</span>
                                 </div>
-                                <span className="subtitle">USD Per Month</span>
+                                <span className="subtitle">Liquid back traditional ERC20 token</span>
                             </div>
                         </div>
                         <div className="pricing-body">
                             <ul className="list-style--1">
-                                <li><FiCheck /> 5 PPC Campaigns</li>
-                                <li><FiCheck /> Keep 100% Royalties</li>
-                                <li><FiCheck /> App Development</li>
-                                <li><FiCheck /> Seo Friendly</li>
-                                <li><FiCheck /> UI/UX designs</li>
+                                <li><FiCheck /> ERC20 Smart Contract</li>
+                                <li><FiCheck /> DEEP Dive Security Audit Report</li>
+                                <li><FiCheck /> Project Github Page</li>
+                                <li><FiCheck /> Minting Dapp</li>
+                                <li><FiCheck /> Custom Contract Dashboard</li>
                             </ul>
                         </div>
                         <div className="pricing-footer">
-                            <a className="btn-default" href="#pricing">Purchase Now</a>
+                            <a className="btn-default" href="./contact">Learn More</a>
                         </div>
                     </div>
                 </div>
@@ -69,29 +66,26 @@ const PricingTwo = () => {
 
             {/* Start PRicing Table Area  */}
             <div className="col-lg-4 col-md-6 col-12">
-                <div className="rn-pricing style-2">
+                <div className="rn-pricing">
                     <div className="pricing-table-inner">
                         <div className="pricing-header">
-                            <h4 className="title">Advanced</h4>
+                            <h4 className="title">ERC20 Token + DAO</h4>
                             <div className="pricing">
                                 <div className="price-wrapper">
                                     <span className="currency">$</span>
-                                    <span className="price">100</span>
+                                    <span className="price">6,500</span>
                                 </div>
-                                <span className="subtitle">USD Per Month</span>
+                                <span className="subtitle">Includes all of ERC Asset Token package plus: </span>
                             </div>
                         </div>
                         <div className="pricing-body">
                             <ul className="list-style--1">
-                                <li><FiCheck /> 50 PPC Campaigns</li>
-                                <li><FiCheck /> SEO Marketing</li>
-                                <li><FiCheck /> Marketing Agency</li>
-                                <li><FiCheck /> Seo Friendly</li>
-                                <li><FiCheck /> Application Manage</li>
+                                <li><FiCheck /> ERC721 DAO Membership Contract</li>
+                                <li><FiCheck /> ERC721 Gated DAO voting page</li>
                             </ul>
                         </div>
                         <div className="pricing-footer">
-                            <a className="btn-default btn-border" href="#pricing">Purchase Now</a>
+                            <a className="btn-default btn-border" href="./contact">Learn More</a>
                         </div>
                     </div>
                 </div>
@@ -100,4 +94,4 @@ const PricingTwo = () => {
         </div>
     )
 }
-export default PricingTwo;
+export default PricingOne;
