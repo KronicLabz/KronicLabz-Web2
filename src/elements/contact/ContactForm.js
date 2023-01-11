ec70939167fa6d45ed1428340524227dbfc7ea02
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-01-11 16:59:44 -0600

    Update ContactForm.js

--- src/elements/contact/ContactForm.js
+++ src/elements/contact/ContactForm.js
@@ -20,16 +20,5 @@ export const ContactUs = () => {
           console.log(error.text);
       });
   };
-    return (
-       <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
-    )
 }
 export default ContactForm;
