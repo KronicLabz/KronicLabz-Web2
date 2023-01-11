13ed2a5ec5ea9586b25e590cef58a76bd798aad9
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-01-11 17:09:49 -0600

    Update ContactForm.js

--- src/elements/contact/ContactForm.js
+++ src/elements/contact/ContactForm.js
@@ -1,11 +1,5 @@
-import React , {useState} from 'react';
-import emailjs from 'emailjs-com';
-
-const Result = () => {
-    return (
-        <p className="success-message">Your Message has been successfully sent. I will contact you soon.</p>
-    )
-}
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 export const ContactUs = () => {
   const form = useRef();
@@ -13,12 +7,23 @@ export const ContactUs = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_98jv9rq', 'template_ckft51c', form.current, 'RhmwyMPbyCzxTTitG')
+    emailjs.sendForm('service_98jv9rq', 'template_jgfr42f', form.current, 'RhmwyMPbyCzxTTitG')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
   };
-}
-export default ContactForm;
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
